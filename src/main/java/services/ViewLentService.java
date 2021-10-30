466d04826e172f4bcf6866d4c5a0956ec79b06a3
Kaleb <kip6163@rit.edu>
2021-10-30 19:48:29 -0400

    overdue lent tools are now indicated when viewLent is used

--- src/main/java/services/ViewLentService.java
+++ src/main/java/services/ViewLentService.java
@@ -7,10 +7,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class ViewLentService {
 
-    private static String FORMAT = "%-12s|%-50s|%-50s|%-12s|%-12s";
+    private static String FORMAT = "%-12s|%-50s|%-50s|%-12s|%-12s|%-15s";
 
     public ViewLentService() {
 
@@ -27,17 +29,21 @@ public class ViewLentService {
             System.out.println("Port Forwarded");
 
             // Do something with the database....
-            String query = "select * from (select barcode, name, description, username, date_responded, status from " +
-                    "(select * from tool_app.tool tool inner join tool_app.request request on request.tool_barcode = " +
+            String query = "select * from (select barcode, name, description, username, date_responded, date_needed_return, " +
+                    "status from (select * from tool_app.tool tool inner join tool_app.request request on request.tool_barcode = " +
                     "tool.barcode) as x where status = 'Accepted') as y order by date_responded asc";
             PreparedStatement statement = conn.prepareStatement(query);
 
             ResultSet result = statement.executeQuery();
 
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            LocalDateTime now = LocalDateTime.now();
+            String currDate = dtf.format(now);      //todays date, for determining if a tool is overdue
+
             if (!result.next()) {
                 System.out.println("No lent tools");
             } else {
-                System.out.format(FORMAT, "barcode", "name", "description", "username", "date_responded");
+                System.out.format(FORMAT, "barcode", "name", "description", "username", "lend date", "return by date");
                 System.out.println();
                 do {
                     String barcode = result.getString("barcode");
@@ -45,10 +51,14 @@ public class ViewLentService {
                     String description = result.getString("description");
                     String username = result.getString("username");
                     String date_responded = result.getString("date_responded");
+                    String date_needed_return = result.getString("date_needed_return");
 
 
 
-                    System.out.format(FORMAT, barcode, name, description, username, date_responded);
+                    System.out.format(FORMAT, barcode, name, description, username, date_responded, date_needed_return);
+                    if(overdue(currDate, date_needed_return)){
+                        System.out.print("   --OVERDUE--  ");
+                    }
                     System.out.println();
                 } while (result.next());
             }
@@ -65,5 +75,13 @@ public class ViewLentService {
                 session.disconnect();
             }
         }
+    }
+
+    public static boolean overdue(String now, String dueDate){
+        if(Integer.parseInt(now.substring(0,4)) == Integer.parseInt(dueDate.substring(0,4))){
+            if(Integer.parseInt(now.substring(5,7)) == Integer.parseInt(dueDate.substring(5,7))){
+                return Integer.parseInt(now.substring(8)) > Integer.parseInt(dueDate.substring(8));
+            } else return Integer.parseInt(now.substring(5, 7)) > Integer.parseInt(dueDate.substring(5, 7));
+        } else return Integer.parseInt(now.substring(0, 4)) > Integer.parseInt(dueDate.substring(0, 4));
     }
 }
